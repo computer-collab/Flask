8033ef7298161ab4506d7738366f6f0136c80e65
computer-collab <computerforgithub@hotmail.com>
2026-02-23 17:55:57 +0530

    Work Saves: Working on profile picture display and enhancing the user experiecne with setting up their own profiles

--- static/js/admin/employees/add_employees.js
+++ static/js/admin/employees/add_employees.js
@@ -1,12 +1,73 @@
-alert("hello world");
-message=document.getElementById("message");
-button = document.querySelectorAll(".radio-label")
-button.forEach(R=> R.addEventListener("click", function() {
-    selected = document.querySelector('input[name="employee_type"]:checked');
-        document.getElementById("message").innerHTML = "You have selected " + selected.value;
-        alert("You have selected " + selected.value);
-    }));    
+// alert("Hello World");
 
-function clicked(){
-    window.location.href = "/admin/employees";
-}
+//JS STARTS HERE
+const  MessageBox = document.getElementById("message");
+const NameBox = document.getElementById("NameBox");
+const EmailBox = document.getElementById("EmailBox");
+const WorkerTypeBox = document.querySelectorAll(".radio-label");
+const ProfilePicInput = document.getElementById("ProfilePicInput");
+const ProfilePic = document.getElementById("ProfilePic");
+const Preview=document.getElementById("Preview");
+const PreviewBackground = document.getElementById("PreviewBackground");
+
+const PreviewButton=document.getElementById("PreviewButton");
+
+
+let  StoredPhoto = null;
+
+
+
+
+WorkerTypeBox.forEach(click =>{
+    click.addEventListener("click",()=>{
+        alert("button is clicked")
+    })
+})
+ProfilePic.addEventListener("click", () => {
+    ProfilePicInput.click();
+});
+
+ProfilePicInput.addEventListener("change", () => {
+
+  const file = ProfilePicInput.files[0];
+
+  if (!file) return;
+
+  preview.src = URL.createObjectURL(file);
+
+});
+PreviewButton.addEventListener('click',(Button)=>{
+PreviewButton.addEventListener('click', () => {
+    if (PreviewBackground.style.display === "block") {
+        PreviewBackground.style.display = "none";
+    } else {
+        PreviewBackground.style.display = "block";
+    }
+});})
+
+
+
+
+
+
+
+let storedPhoto = null;
+
+const input = document.getElementById("ProfilePicInput");
+const preview = document.getElementById("Preview");
+
+input.addEventListener("change", function(){
+
+    storedPhoto = input.files[0];
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+
+        preview.src = e.target.result;
+
+    }
+
+    reader.readAsDataURL(storedPhoto);
+
+});
